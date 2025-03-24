27553edf4661dc575b10b9686e4588d91360832d
Ayub-002 <ayub.ndungu@student.moringaschool.com>
2025-03-24 07:59:18 +0300

    adds arithimetics

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
 
 // Write your code here
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
+
+console.log(multiply);  // Output: 62
+
+
+let random = Math.floor(Math.random() * 10) + 1
+console.log(random)
+
+let mod = (num3 % num4)
+mod = 4
+let num3 = 16
+let num4 = 6
+console.log(mod)
+
+let max = Math.max(1, 2, 3, 4, 20)
+console.log(max)
